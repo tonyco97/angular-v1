832988433ea55d223ad167fca3eacdec93771eea
Achille Astolfi <achille.astolfi@example.com>
2020-11-19 00:57:33 +0100

    index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "index", component: IndexComponent },
+  // default
+  { path: "", pathMatch: "full", redirectTo: "/index" },
+  // catch-all
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
